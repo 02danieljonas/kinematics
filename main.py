cbf4606f56ce2b569d5bba37c1e8e37ebeca489f
Daniel <02danieljonas@gmail.com>
2022-02-01 20:33:07 -0500

    Updated the visuals

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import numpy as np
 import matplotlib
 
 fig, ax = plt.subplots()
+
 def setUp():
     ax.plot([0, 1, 3], [0, 1, 3], color="white")
 
 def update():
-    setUp()
     plt.cla()
+
+    setUp()
+    ax.plot(xSegment, ySegment)
     plt.show()
 
 setUp()
@@ -18,19 +21,21 @@ xSegment = [1.5, 1.5]
 ySegment = [0, 2]
 ax.plot(xSegment, ySegment)
 
-def withinBounds(xposition, ypositon):
-    if xposition is not None:
-        if xposition > 2.99999:
-            xposition = 2.99999
-        elif xposition <.00001:
-            xposition = .00001
+def withinBounds(xPosition, yPositon):
+    if xPosition is not None:
+        if xPosition > 2.99999:
+            xPosition = 2.99999
+        elif xPosition <.00001:
+            xPosition = .00001
 
-    if ypositon is not None:
-        if ypositon > 2.99999:
-            ypositon = 2.99999
-        elif ypositon < .00001:
-            ypositon = .00001
-    return xposition, ypositon
+    if yPositon is not None:
+        if yPositon > 2.99999:
+            yPositon = 2.99999
+        elif yPositon < .00001:
+            yPositon = .00001
+    return xPosition, yPositon
+
+
 
 def on_move(data):
     xdata=data.xdata
@@ -40,12 +45,7 @@ def on_move(data):
 
     print(xSegment,ySegment)
 
-    plt.cla()
-
-    setUp()
-    ax.plot(xSegment, ySegment)
-
-    plt.show()
+    update()
 
 
 binding_id = plt.connect('motion_notify_event', on_move)
