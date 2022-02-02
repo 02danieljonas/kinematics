91ba67bed3ed9e98a267fad46d1b16beea7d47eb
Daniel <02danieljonas@gmail.com>
2022-02-01 20:41:25 -0500

    Removed unnecessary code and made it look better

--- main.py
+++ main.py
@@ -17,34 +17,35 @@ def update():
 
 setUp()
 
+
+
+
 xSegment = [1.5, 1.5]
 ySegment = [0, 2]
 ax.plot(xSegment, ySegment)
 
 def withinBounds(xPosition, yPositon):
-    if xPosition is not None:
-        if xPosition > 2.99999:
-            xPosition = 2.99999
-        elif xPosition <.00001:
-            xPosition = .00001
+    if xPosition > 2.99999:
+        xPosition = 2.99999
+    elif xPosition <.00001:
+        xPosition = .00001
 
-    if yPositon is not None:
-        if yPositon > 2.99999:
-            yPositon = 2.99999
-        elif yPositon < .00001:
-            yPositon = .00001
+    if yPositon > 2.99999:
+        yPositon = 2.99999
+    elif yPositon < .00001:
+        yPositon = .00001
+
     return xPosition, yPositon
 
 
 
 def on_move(data):
-    xdata=data.xdata
-    ydata=data.ydata
+    if data.xdata is not None:
+        xdata=data.xdata
+    if data.ydata is not None:
+        ydata=data.ydata
 
     xSegment[1], ySegment[1] = withinBounds(xdata, ydata)
-
-    print(xSegment,ySegment)
-
     update()
 
 
