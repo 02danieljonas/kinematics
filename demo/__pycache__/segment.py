10374500e53f3ad169d7319ad255aec9887af83a
Daniel <02danieljonas@gmail.com>
2022-02-03 21:57:03 -0500

    copied his code

---
+++ demo/__pycache__/segment.py
@@ -0,0 +1,9 @@
+class Segment():
+    def __init__(self, position1, position2, len, angle):
+        self.position1 = position1
+        self.position2 = position2
+        self.len = len
+        self.angle = angle
+    
+    def show():
+        
--- demo/main.py
+++ demo/main.py
@@ -1,10 +1,14 @@
-import sys, pygame, time, random, tentacle, testing
+import sys, pygame, time, random, tentacle, math
 from random import randint
+
+
 pygame.init()
 
 
 FPS=60
+SPEED=10
 WIDTH, HEIGHT = 900,500
+middleX, middleY=900/2,500/2
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Kinematics")
 background_color = (0,0,0)
@@ -15,9 +19,22 @@ def draw_window():
     pygame.display.update()
     screen.fill(background_color)
 
+class Segment():
+    def __init__(self, position1, len, angle):
+        self.position1 = position1
+        self.len = len
+        self.angle = math.radians(angle)
+        dx= len * math.cos(self.angle)
+        dy= len * math.sin(self.angle)
+        self.position2 = (self.position1[0]+dx, self.position1[1]+dy)
+
+    def show(self):
+        pygame.draw.line(screen, (0,255,255), self.position1, self.position2)
+
 
 def make_thing(x, y):
-    pygame.draw.rect(screen, (0,255,255), pygame.Rect(x, y, 1, 39))
+    jack = Segment((x, y), 40, 90)
+    jack.show()
 
 
 def main():
@@ -25,7 +42,6 @@ def main():
     while run:
         pygame.time.Clock().tick(FPS)
         x, y =pygame.mouse.get_pos()
-        print(x)
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 run = False
@@ -36,3 +52,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+#I want 
