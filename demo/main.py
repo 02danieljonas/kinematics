da13a258c62b3fb59b6cc5c0ad7a29a0c5c88c2d
Daniel <02danieljonas@gmail.com>
2022-02-09 10:14:13 -0500

    Made the moving animation, TODO: make it follow a straight line, instead of moving x and y independently, use maths from HS

--- demo/main.py
+++ demo/main.py
@@ -1,13 +1,11 @@
 import sys, pygame, time, random, tentacle, math
 from random import randint
 
-
 pygame.init()
-
 
 FPS=600
 SPEED=10
-WIDTH, HEIGHT = 900,500
+WIDTH, HEIGHT = 900,900
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Kinematics")
 background_color = (0,0,0)
@@ -22,43 +20,27 @@ def draw_window():
 
 
 class Segment():
-    def __init__(self, *args):#(self, xPos, yPos, length, angle)  or (self, master, length, angle)
+    def __init__(self, *args):#(self, xPos, yPos, length, angle, speed)  or (self, master, length, angle, speed)
         
-        if len(args)==4:
+        if len(args)==5:
             self.list = [self]
             self.xPos = args[0]
             self.yPos = args[1]
             self.length = args[2]
-            
-            
-            
+            self.speed = args[4]
             self.angle = math.radians(args[3])
-            
             self.xVariation, self.yVariation, self.xPos2, self.yPos2 = self.findPos2(self.xPos, self.yPos, self.length, self.angle)
             
-            # self.xVariation= self.length * math.cos(self.angle)
-            # self.yVariation= self.length * math.sin(self.angle)
-            
-            # self.xPos2 = self.xPos+self.xVariation
-            # self.yPos2 = self.yPos+self.yVariation
-
             pygame.draw.line(screen, (0,255,255), (self.xPos, self.yPos), (self.xPos, self.yPos2))
 
-        elif len(args)==3:
+        elif len(args)==4:
             args[0].list.append(self)
             self.xPos = args[0].xPos2
             self.yPos = args[0].yPos2
             self.length = args[1]
-            
+            self.speed = args[3]
             self.angle = math.radians(args[2])
-
             self.xVariation, self.yVariation, self.xPos2, self.yPos2 = self.findPos2(self.xPos, self.yPos, self.length, self.angle)
-            
-            # self.xVariation= self.length * math.cos(self.angle)
-            # self.yVariation= self.length * math.sin(self.angle)
-            
-            # self.xPos2 = self.xPos+self.xVariation
-            # self.yPos2 = self.yPos+self.yVariation
 
             pygame.draw.line(screen, (0,255,255), (self.xPos, self.yPos), (self.xPos2, self.yPos2))
 
@@ -67,37 +49,45 @@ class Segment():
         A = length * math.cos(angle)
         B = length * math.sin(angle)
         return A, B, X+A, Y+B
-        self.xVariation, self.xVariation, self.xPos2, self.yPos2
 
 
+    def move(self, xMoveTo, yMoveTo):
+        if self.xPos-xMoveTo<=-self.speed:
+            self.xPos+=self.speed
+        elif self.xPos-xMoveTo>=self.speed:
+            self.xPos-=self.speed
+        else:
+            self.xPos=xMoveTo
+        
+        if self.yPos-yMoveTo<=-self.speed:
+            self.yPos+=self.speed
+        elif self.yPos-yMoveTo>=self.speed:
+            self.yPos-=self.speed
+        else:
+            self.yPos=yMoveTo
 
-    def move(x, y):
-        pass
-        #to move self.xpos += speed to which ever direction you want
-        #to move self.ypos += speed to which ever direction you want
 
     def show(self, x, y):
-        for item in self.list:
-            item.xPos = x
-            item.yPos = y
-            x = item.xPos2 = item.xPos+item.xVariation
-            y = item.yPos2 = item.yPos+item.yVariation
-            pygame.draw.line(screen, (0,255,255), (item.xPos, item.yPos), (item.xPos2, item.yPos2))
+        for self in self.list:
+            #self.move() should be here
+            self.move(x, y)
+            
+            # item.xPos = x
+            # item.yPos = y
+            x = self.xPos2 = self.xPos+self.xVariation
+            y = self.yPos2 = self.yPos+self.yVariation
+            pygame.draw.line(screen, (0,255,255), (self.xPos, self.yPos), (self.xPos2, self.yPos2))
 
 
-jack = Segment(0, 0, 40, 45)
-steve = Segment(jack, 40, 90)
+jack = Segment(0, 0, 40, 45, 6)
+steve = Segment(jack, 40, 90, 6)
 
-steven = Segment(0, 0, 40, 0)
-maker = Segment(steven, 40, 0)
+steven = Segment(0, 0, 40, 0, 6)
+maker = Segment(steven, 40, 0, 6)
 
 def make_thing(x, y):
     jack.show(x, y)
     steven.show(x, y)
-
-
-
-
 
 
 def main():
