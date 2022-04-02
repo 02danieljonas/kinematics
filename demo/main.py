91c3a5cb70fb16d1a2c6cf40e2f51225ec02dc36
Daniel Jonas <02danieljonas@gmail.com>
2022-04-02 16:18:53 -0400

    fixed all previous bugs, angles are not relative!!!! :D

--- demo/main.py
+++ demo/main.py
@@ -16,13 +16,15 @@ LENGTH = None
 
 class Segment():
     def __init__(self, *args):#(self, xPos, yPos, length, angle, speed)  or (self, captain, parent, length, angle, speed)
-        
+        """To create the starting segment pass the XPosition Yposition, the length, angle and speed
+        To create a child pass a starting segment, length, angle and speed
+        """        
         if isinstance(args[0], float) or isinstance(args[0], int): #(self, xPos, yPos, length, angle, speed)
             self.list = [self]
             self.xPos = args[0]
             self.yPos = args[1]
             self.length = args[2]
-            self.speed = args[4]
+            self.speed = args[4]/200
             self.angle = math.radians(args[3])
             self.findPos2()            
             pygame.draw.line(screen, (0,255,255), (self.xPos, self.yPos), (self.xPos, self.yPos2))
@@ -34,103 +36,43 @@ class Segment():
             self.xPos = self.parent.xPos2
             self.yPos = self.parent.yPos2
             self.length = args[1]
-            self.speed = args[3]
-            self.angle = math.radians(args[2])
+            self.speed = args[3]/200
+
+            self.realAngle = math.radians(args[2])#this is the actualy angle value
+            self.angle = self.realAngle + self.parent.angle#this is the angle relative to its parent
+
             self.findPos2()
             pygame.draw.line(screen, (0,255,255), (self.xPos, self.yPos), (self.xPos2, self.yPos2))
         else:
             raise Exception("Incorrect Format")
 
-
     def findPos2(self):
-        self.xVariation = self.length * math.cos(self.angle)
-        self.yVariation = self.length * math.sin(self.angle)
-        self.xPos2=self.xVariation+self.xPos
-        self.yPos2=self.yVariation+self.yPos
+        xVariation = self.length * math.cos(self.angle)
+        yVariation = self.length * math.sin(self.angle)
+        self.xPos2=xVariation+self.xPos
+        self.yPos2=yVariation+self.yPos
     
-    
-    def findObject(self, xTarget, yTraget):
-        if hasattr(self, "parent"):
-            self.xPos=self.parent.xPos2
-            self.yPos=self.parent.yPos2
-            self.angle += self.speed + self.parent.angle
-        else:
-            self.angle +=self.speed
+    def followParent(self):
+        self.xPos = self.parent.xPos2
+        self.yPos = self.parent.yPos2
+        self.realAngle+=self.speed
+        print(self.speed)
+        self.angle = self.realAngle + self.parent.angle
         self.findPos2()
-
-    def findVectorAngle(self, xTarget, yTarget):
-
-        # print("xTarget2 is ", xTarget)
-        # print("yTraget2 is ", yTarget)
-
-        u = (self.xPos-self.xPos+0.0000000001, self.yPos-self.yPos+0.0000000001)
-        # print("us is ", u)
-        v = (self.xPos2-xTarget+0.0000000001, self.xPos2-yTarget+0.0000000001)
-        # print("v is ", v)
-
-        uv = (u[0]*v[0]+u[1]*v[1])
-        # print("uv is ", uv)
-
-        uMagnitude = math.sqrt(math.pow(u[0],2) + math.pow(u[1], 2))
-        # print("uMagnitude is ", uMagnitude)
-
-        vMagnitude = math.sqrt(math.pow(v[0],2) + math.pow(v[1], 2))
-        # print("vMagnitude is ", vMagnitude)
-
-        self.angle = math.degrees(math.acos(uv/(uMagnitude*vMagnitude+0.0000000001)))
-        # print("self.angle is ", self.angle)
-
-
-    def move(self, xMoveTo, yMoveTo):
-        if (self.yPos-yMoveTo)!=0 and (self.xPos-xMoveTo)!=0:
-            # print(self.yPos-yMoveTo, self.xPos-xMoveTo)
-            slope = (self.yPos-yMoveTo)/(self.xPos-xMoveTo)
-            #TODO: make it follow a straight line, instead of moving x and y independently, use maths from HS
-            if self.xPos-xMoveTo<=-self.speed: LorR=1
-                # self.xPos+=self.speed #moves it to the right
-            elif self.xPos-xMoveTo>=self.speed: LorR=-1
-                # self.xPos-=self.speed #moves it to the left
-            else: LorR=0
-                # self.xPos=xMoveTo #teleports it to position
-                
-            
-            if self.yPos-yMoveTo<=-self.speed:UorD=1
-                # self.yPos+=self.speed #moves it up
-            elif self.yPos-yMoveTo>=self.speed:UorD=-1
-                # self.yPos-=self.speed #moves it down
-            else:UorD=0
-                # self.yPos=yMoveTo #teleports it to position
-
-            if LorR == 1 and UorD == 1:
-                pass
-        else:
-            self.xPos=xMoveTo #teleports it to position
-            self.yPos=yMoveTo #teleports it to position
-
-
-    def show(self, x, y):
+    
+    def update(self):
         for self in self.list:
-            # print("x is ", x)
-            # print("y is ", y)
-            
-            self.findObject(x, y)
-            
-            x = self.xPos2
-            y = self.yPos2
-            pygame.draw.line(screen, (0,255,255), (self.xPos, self.yPos), (self.xPos2, self.yPos2))
+            if not hasattr(self, "captain"):
+                self.angle+=self.speed
+                self.findPos2()
+                pygame.draw.line(screen, (0,255,255), (self.xPos, self.yPos), (self.xPos2, self.yPos2))
+            else:
+                self.followParent()
+                self.findPos2()
+                pygame.draw.line(screen, (0,255,0), (self.xPos, self.yPos), (self.xPos2, self.yPos2))
 
-
-jack = Segment(200, 100, 40, 270, 0)
-pete = Segment(jack, 40, 90, .0)
-# peter = Segment(jack, pete, 10, 0, .0)
-# peter = Segment(jack, peter, 20, 10, .0)
-# peter = Segment(jack, peter, 40, 20, .0)
-# peter = Segment(jack, peter, 60, 30, .0)
-
-
-# steve = Segment(WIDTH/4, HEIGHT/2, 40, 45, .01)
-# stev = Segment(steve, 40, 90, .01)#found an error can't connect code like this
-
+jack = Segment(WIDTH/2, HEIGHT/2, 40, 90, 2)
+pete = Segment(jack, 50, 90, 0)
 
 def draw_window():
     global background_color
@@ -138,7 +80,7 @@ def draw_window():
     screen.fill(background_color)
 
 def make_thing(x, y):
-    jack.show(x, y)
+    jack.update()
 
 def main():
     run = True
@@ -155,7 +97,7 @@ def main():
         draw_window()
         # print("The first values are ", x, y)
         make_thing(x, y)
-        if pygame.display.get_active() and (time()-startTime>4):
+        if (not pygame.display.get_active()) and (time()-startTime>4):
             run=False
 
     pygame.quit()
--- demo/tentacle.py
+++ demo/tentacle.py
@@ -1,29 +1,31 @@
 import math
 
-xPos = -2
-xPos2 = 4
+print(math.radians(360))
 
-u = (-2, 5)
-v = (0.00000001, 0.000000001)
-uv = (u[0]*v[0]+u[1]*v[1])
-print(uv)
-uMagnitude = math.sqrt(math.pow(u[0],2) + math.pow(u[1], 2))
-print(uMagnitude)
+# xPos = -2
+# xPos2 = 4
 
-vMagnitude = math.sqrt(math.pow(v[0],2) + math.pow(v[1], 2))
-print(vMagnitude)
+# u = (-2, 5)
+# v = (0.00000001, 0.000000001)
+# uv = (u[0]*v[0]+u[1]*v[1])
+# print(uv)
+# uMagnitude = math.sqrt(math.pow(u[0],2) + math.pow(u[1], 2))
+# print(uMagnitude)
 
-print(uMagnitude*vMagnitude)
+# vMagnitude = math.sqrt(math.pow(v[0],2) + math.pow(v[1], 2))
+# print(vMagnitude)
 
-print(math.degrees(math.acos(uv/(uMagnitude*vMagnitude))))
+# print(uMagnitude*vMagnitude)
+
+# print(math.degrees(math.acos(uv/(uMagnitude*vMagnitude))))
 
 
 
-class Segments:
-    def __init__(self, num_segments, len_segment, thick_segment):
-        self.num_segments = num_segments
-        self.len_segment = len_segment
-        self.thick_segment = thick_segment
-    def move(self, xposition, yposition):
-        pass
+# class Segments:
+#     def __init__(self, num_segments, len_segment, thick_segment):
+#         self.num_segments = num_segments
+#         self.len_segment = len_segment
+#         self.thick_segment = thick_segment
+#     def move(self, xposition, yposition):
+#         pass
     
